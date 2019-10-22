570cd45b0ff7d803df431589bc196a5a00080ae1
George Moore <gdubya1218@gmail.com>
2019-10-22 15:11:51 -0400

    complete objective1lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "Name  : George Moore" );
+    System.out.println( "Age   : 36 ");
+    System.out.println( "State : NH");
+  }
+}
