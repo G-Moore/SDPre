c154f5d7284a587ae853663d0bb20ca0bb3e617d
George Moore <gdubya1218@gmail.com>
2019-10-22 15:40:08 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2lab2
+{
+  public static void main( String[] args )
+  {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    System.out.println( "Name: " + name + "\tAge: " + age + "\tState: " + state);
+  }
+}
