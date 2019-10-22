d9bd44542eb44903d3a838adfb39fa739d2153c6
George Moore <gdubya1218@gmail.com>
2019-10-22 14:30:42 -0400

    complete objective1lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,12 +1,7 @@
 public class Objective1Lab1
 {
-    public static void main(String[] args)
+    public static void main( String[] args )
     {
-
-      /*
-        Your solution goes here
-      */
       System.out.println( "George Moore" );
-
     }
   }
