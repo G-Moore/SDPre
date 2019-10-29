bd047ae6c1fb36f6ac0894f7c85ca76707439d35
George Moore <gdubya1218@gmail.com>
2019-10-29 16:14:09 -0400

    complete objective6lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,18 @@
+public class Objective6Lab4 {
+  public static void main (String[]args) {
+    int count, sum;
+    count = 0;
+    sum = 0;
+
+      while ( count <= 20 ) {
+        if ( count < 20){
+
+        }
+        else {
+          System.out.println(sum);
+        }
+        count = count + 1;
+        sum = sum + count;
+    }
+  }
+}
